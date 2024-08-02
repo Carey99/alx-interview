668c7252b4773bc527cae87241dca86cc213c76e
Carey99 <edwinakidah1@gmail.com>
2024-08-02 05:57:11 +0300

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    nqueens problem
+"""
+import sys
+
+
+def isSafe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
